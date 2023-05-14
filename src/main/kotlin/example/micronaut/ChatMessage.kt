8feaf49728b940b43d08c505aae7f02c4205612e
kuka666 <kuka666@github.com>
2023-05-14 23:54:45 +0600

    APi change

--- src/main/kotlin/example/micronaut/ChatMessage.kt
+++ src/main/kotlin/example/micronaut/ChatMessage.kt
@@ -1,5 +1,6 @@
 package example.micronaut
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.bson.codecs.pojo.annotations.BsonCreator
 import org.bson.codecs.pojo.annotations.BsonProperty
 import java.time.Instant
@@ -10,8 +11,14 @@ data class ChatMessage @BsonCreator constructor(
     @BsonProperty("senderId") val senderId: String,
     @BsonProperty("receiverId") val receiverId: String,
     @BsonProperty("message") val message: String,
-    @BsonProperty("sentTime") val sentTime: Instant
+    @BsonProperty("sentTime") val sentTime: Instant,
+    @BsonProperty("referredMessageId") val referredMessageId: String,
+    @BsonProperty("referredMessageText") val referredMessageText: String
 ){
-    constructor(senderId: String, receiverId: String, message: String, sentTime: Instant) : this(null, senderId, receiverId, message, sentTime)
+    constructor(senderId: String, receiverId: String, message: String, sentTime: Instant, referredMessageId: String, referredMessageText: String) : this(null, senderId, receiverId, message, sentTime, referredMessageId, referredMessageText)
 }
-
+data class ChatMessageDTO(
+    @JsonProperty("text") val text: String,
+    @JsonProperty("referredMessageText") val referredMessageText: String?,
+    @JsonProperty("referredMessageId") val referredMessageId: String?
+)
